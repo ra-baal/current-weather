5040a75b5c08dcba04e9fc172be93b110be62fff
rabaal <balcerowskirafal@gmail.com>
2021-08-30 19:06:35 +0200

    Project created.

---
+++ GetCurrentWeather/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetCurrentWeather
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
