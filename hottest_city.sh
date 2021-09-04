e50b4a08dcbc0032fd9d2574c38060895025ae6a
rabaal <balcerowskirafal@gmail.com>
2021-09-04 21:27:51 +0200

    Bash sorting.

--- hottest_city.sh
+++ hottest_city.sh
@@ -2,7 +2,7 @@
 
 LC_ALL=C
 
-if [[ -z "$GET_CURRENT_WEATHER_BIN}" ]]
+if [[ -z "$GET_CURRENT_WEATHER_BIN" ]]
 then
 	echo "The GET_CURRENT_WEATHER_BIN variable is not set."
 	exit 1
@@ -12,37 +12,20 @@ get_current_weather () {
 	result=`${GET_CURRENT_WEATHER_BIN} --city "$1" --units metric`
 	if (( $? == 0 ))
 	then
-		echo $result
+		echo "$1" "("`echo $result | cut -d "|" -f 2`")"
 	else
 		echo -n
 	fi
 }
 
-results=
-{   
+#results=
+{
 	while read city
 	do
 		get_current_weather "$city" &
 	done
 
 	wait
-
-} | sort -t "|" -k 2,2 -g -r | head -n 3
-
-ifs=$IFS
-IFS='\n'
-for r in $results
-do
-	echo "$r" | cut -d "|" -f 2 # doesnt work properly !!!
-done
-IFS=$ifs
-
-
-
-#declare -A array
-
-#array[$city]=`echo "$result" | cut -d "|" -f 2`
-#echo "$city (${array[$city]})"
-
+} | sort -r -g -t "(" -k 2 | head -n 3
 
 exit 0
